a07e666b0c2451874c6d637466f6904ba9787e4a
todorz4 <152098036+todorz4@users.noreply.github.com>
2023-11-26 21:28:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+var blok1 = document.querySelector('#projekat1');
+function redirect1(){
+    window.location.href="https://todorz4.github.io/project1/";
+}
+blok1.addEventListener('click',redirect1);
+
+
+var blok2 = document.querySelector('#projekat2');
+function redirect2(){
+    window.location.href="https://todorz4.github.io/selidbe/";
+}
+blok2.addEventListener('click',redirect2);
+
+
+var blok3 = document.querySelector('#projekat3');
+function redirect3(){
+    window.location.href="https://todorz4.github.io/malani/";
+}
+blok3.addEventListener('click',redirect3);
+
+var blok4 = document.querySelector('#projekat4');
+
+function redirect4(){
+    window.location.href="https://fashion-park.000webhostapp.com/index.php";
+}
+
+blok4.addEventListener('click',redirect4);
